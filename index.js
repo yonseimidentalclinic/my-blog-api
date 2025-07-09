dc479b308add9f8ed1a0b0a5601cfdb1d9465f46
gun hyun park <pghdent43@gmail.com>
2025-07-09 16:34:14 +0900

    수정

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const initializeDatabase = async () => {
                 id SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL
+                role VARCHAR(50) NOT NULL DEFAULT 'user'
             );
         `);
         await client.query(`
